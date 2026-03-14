ca7f826e544804c6674c425d2a1e1d65b1ee4336
BuhDuy <nbduy23@clc.fitus.edu.vn>
2026-03-14 16:00:07 +0700

    improve schedule job

--- backend/src/main/java/com/walkmate/application/ProcessSessionActivationWindowWorker.java
+++ backend/src/main/java/com/walkmate/application/ProcessSessionActivationWindowWorker.java
@@ -2,12 +2,14 @@ package com.walkmate.application;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "app.scheduler.enabled", havingValue = "true")
 public class ProcessSessionActivationWindowWorker {
   private final ProcessSessionActivationWindowService service;
 
